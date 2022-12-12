6035f5b1d7b6e920024b300fe4df68d6119665d0
Marek Fodor <fodormarek5@gmail.com>
2022-12-12 15:14:54 +0100

    init

---
+++ prettier.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  overrides: [
+    {
+      files: "*",
+      options: {
+        trailingComma: "all",
+        printWidth: 120,
+        tabWidth: 2,
+        singleQuote: true,
+        semi: true,
+        jsxSingleQuote: false,
+        bracketSameLine: false,
+      },
+    },
+  ],
+};
