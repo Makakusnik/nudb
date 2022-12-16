f3fd1b8cf1906af70ebede11291148e399a81568
Marek Fodor <fodormarek5@gmail.com>
2022-12-16 17:06:11 +0100

    Move child only type

--- src/components/TableComponents/index.tsx
+++ src/components/TableComponents/index.tsx
@@ -1,8 +1,4 @@
-import type { ReactNode } from 'react';
-
-interface ChildrenProps {
-  children: ReactNode | ReactNode[];
-}
+import type { ChildrenProps } from '@type/index';
 
 export const TableRow = ({ children }: ChildrenProps) => {
   return <tr className="flex w-full flex-row py-1 first:mt-0 even:bg-slate-100 lg:rounded-lg">{children}</tr>;
@@ -17,9 +13,9 @@ export const NutrientTableCell = ({ children }: ChildrenProps) => {
 };
 
 export const NameTableCell = ({ children }: ChildrenProps) => {
-  return <td className="min-w-[12rem] max-w-[12rem] py-2 pl-4 md:min-w-[16rem] md:max-w-[16rem]">{children}</td>;
+  return <td className="min-w-[12rem] max-w-[12rem] py-2 px-0 md:min-w-[16rem] md:max-w-[16rem]">{children}</td>;
 };
 
 export const CheckboxTableCell = ({ children }: ChildrenProps) => {
-  return <td className="inline-flex items-center py-2 pl-4">{children}</td>;
+  return <td className="inline-flex items-center py-2 px-4">{children}</td>;
 };
