28f77d59058f51c94b52b0db14fbbf9b98178a66
Marek Fodor <fodormarek5@gmail.com>
2022-12-21 16:15:30 +0100

    Tweak keyboard handler to high order function

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -6,39 +6,43 @@ interface KeyboardEventCombo {
   handler: (e?: KeyboardEvent) => void;
 }
 
-export const keyboardEventHandler = (e: KeyboardEvent, data: KeyboardEventCombo | KeyboardEventCombo[]): void => {
-  // Data is array
-  if (Array.isArray(data)) {
-    data.forEach(({ keys, handler }) => {
-      // Multiple keys
-      if (Array.isArray(keys)) {
-        keys.forEach((key) => {
-          if (key === e.key) {
+type EventReturn = (e: KeyboardEvent) => void;
+
+export const keyboardEventHandler = (data: KeyboardEventCombo | KeyboardEventCombo[]): EventReturn => {
+  return (e) => {
+    // Data is array
+    if (Array.isArray(data)) {
+      data.forEach(({ keys, handler }) => {
+        // Multiple keys
+        if (Array.isArray(keys)) {
+          keys.forEach((key) => {
+            if (key === e.key) {
+              handler(e);
+            }
+          });
+        } else {
+          // Single key
+          if (keys === e.key) {
             handler(e);
+          }
+        }
+      });
+    }
+    // Data is single object
+    else {
+      // Multiple keys
+      if (Array.isArray(data.keys)) {
+        data.keys.forEach((key) => {
+          if (key === e.key) {
+            data.handler(e);
           }
         });
       } else {
         // Single key
-        if (keys === e.key) {
-          handler(e);
-        }
-      }
-    });
-  }
-  // Data is single object
-  else {
-    // Multiple keys
-    if (Array.isArray(data.keys)) {
-      data.keys.forEach((key) => {
-        if (key === e.key) {
+        if (data.keys === e.key) {
           data.handler(e);
         }
-      });
-    } else {
-      // Single key
-      if (data.keys === e.key) {
-        data.handler(e);
       }
     }
-  }
+  };
 };
