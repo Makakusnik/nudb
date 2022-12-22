c3c0af8ed575c221a3f90e0a8e4e0f095a15969a
Marek Fodor <fodormarek5@gmail.com>
2022-12-22 12:03:16 +0100

    Add Food add modal content

--- src/components/Modal/Data/index.tsx
+++ src/components/Modal/Data/index.tsx
@@ -20,12 +20,100 @@ interface FoodDetailProps {
   product: Product;
 }
 
+const NutritionTable = ({ data }: { data: Product }) => {
+  return (
+    <div className="flex min-w-full">
+      <table className="flex w-full flex-col">
+        <caption className="hidden">Nutritional Values for {data.name}</caption>
+        <tbody className="w-full">
+          <tr className="flex w-full justify-between py-1 ">
+            <th className="font-medium" scope="row">
+              Fats
+            </th>
+            <td>{data.fats ?? 0} g</td>
+          </tr>
+          {data.saturatedFats && (
+            <tr className="flex w-full justify-between ">
+              <th className="ml-4" scope="row">
+                <abbr title="Saturated Fatty Acids">SFA</abbr>
+              </th>
+              <td>{data.saturatedFats ?? 0} g</td>
+            </tr>
+          )}
+          {data.monoUnsaturatedFats && (
+            <tr className="flex w-full justify-between ">
+              <th className="ml-4" scope="row">
+                <abbr title="Mono Unsaturated Fatty Acids">MUFA</abbr>
+              </th>
+              <td>{data.monoUnsaturatedFats ?? 0} g</td>
+            </tr>
+          )}
+          {data.polyUnsaturatedFats && (
+            <tr className="flex w-full justify-between ">
+              <th className="ml-4" scope="row">
+                <abbr title="Poly Unsaturated Fatty Acids">PUFA</abbr>
+              </th>
+              <td>{data.polyUnsaturatedFats ?? 0} g</td>
+            </tr>
+          )}
+          <tr className="flex w-full justify-between ">
+            <th className="font-medium" scope="row">
+              Carbohydrates
+            </th>
+            <td>{data.carbohydrates ?? 0} g</td>
+          </tr>
+          <tr className="flex w-full justify-between ">
+            <th className="ml-4 " scope="row">
+              Sugar
+            </th>
+            <td>{data.sugar ?? 0} g</td>
+          </tr>
+          <tr className="flex w-full justify-between ">
+            <th className="ml-4 " scope="row">
+              Fiber
+            </th>
+            <td>{data.fiber ?? 0} g</td>
+          </tr>
+          <tr className="flex w-full justify-between ">
+            <th className="font-medium" scope="row">
+              Proteins
+            </th>
+            <td>{data.proteins ?? 0} g</td>
+          </tr>
+          <tr className="flex w-full justify-between ">
+            <th className="font-medium" scope="row">
+              Salt
+            </th>
+            <td>{data.salt ?? 0} g</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
 export const FoodDetailModal = ({ product }: FoodDetailProps) => {
   const { setModalState } = useContext(ModalContext);
   return (
-    <Dialog.Panel className={'w-full max-w-sm rounded-md bg-white p-4'}>
-      <Dialog.Title>{product.name}</Dialog.Title>
-      <Dialog.Description>{product.fats}</Dialog.Description>
+    <Dialog.Panel className={'w-full max-w-2xl rounded-md bg-white p-6'}>
+      <Dialog.Title className={'text-2xl font-medium'}>
+        {product.name}
+        <span className="ml-1 text-base font-normal">{product.brand}</span>
+      </Dialog.Title>
+      <Dialog.Description className="mt-4 flex" as="div">
+        <div className="flex justify-between">
+          <div className="tooltip-top flex w-full flex-col bg-slate-500" data-tooltip="This feature is in development">
+            <h3 className="text-xl font-normal">Prices</h3>
+            <div className="flex"></div>
+          </div>
+          <div className="flex w-full max-w-[16rem] flex-col px-4">
+            <h3 className="mb-2 text-xl font-normal">Nutrition Values</h3>
+            <div className="flex w-full max-w-screen-xs">
+              <NutritionTable data={product}></NutritionTable>
+            </div>
+          </div>
+        </div>
+      </Dialog.Description>
       <button onClick={() => setModalState && setModalState(false)}>Deactivate</button>
       <button onClick={() => setModalState && setModalState(false)}>Cancel</button>
     </Dialog.Panel>
