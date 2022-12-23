c486c57bad4f19638f2d737d8ab1bebfac421d41
Marek Fodor <fodormarek5@gmail.com>
2022-12-23 14:46:10 +0100

    Add top z-index

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -7,10 +7,9 @@ import { DefaultModal } from './Data';
 
 export const Modal = () => {
   const { ModalContent, isModalOpen, setModalState } = useContext(ModalContext);
-
   return (
     <Dialog open={isModalOpen} onClose={() => setModalState && setModalState(false)}>
-      <div className="fixed inset-0 flex items-center justify-center bg-black/30 p-4">
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/30 p-4">
         {ModalContent || <DefaultModal />}
       </div>
     </Dialog>
