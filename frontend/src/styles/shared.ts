a4da59d64281b7eeb72245be283ea0e14c569dac
Marek Fodor <fodormarek5@gmail.com>
2022-12-29 23:25:56 +0100

    Add shared focus styles + types

---
+++ frontend/src/styles/shared.ts
@@ -0,0 +1,13 @@
+export const focusGreen = `focus:ring-4 focus:ring-inset focus:ring-green-400`;
+export const focusTeal = `focus:ring-4 focus:ring-inset focus:ring-teal-400`;
+export const focusYellow = `focus:ring-4 focus:ring-inset focus:ring-yellow-400`;
+export const focusRed = `focus:ring-4 focus:ring-inset focus:ring-red-400`;
+export const focusBlue = `focus:ring-4 focus:ring-inset focus:ring-blue-400`;
+
+export const colorsGreen = 'bg-green-300 p-3 text-green-600 hover:bg-green-200 hover:text-green-500';
+export const colorsTeal = 'bg-teal-300 p-3 text-teal-600 hover:bg-teal-200 hover:text-teal-500';
+export const colorsYellow = 'bg-yellow-300 p-3 text-yellow-600 hover:bg-yellow-200 hover:text-yellow-500';
+export const colorsRed = 'bg-red-300 p-3 text-red-600 hover:bg-red-200 hover:text-red-500';
+export const colorsBlue = 'bg-blue-300 p-3 text-blue-600 hover:bg-blue-200 hover:text-blue-500';
+
+export const disabledStyle = `disabled:bg-slate-300 disabled:text-slate-600`;
---
+++ frontend/src/types/Styles/index.ts
@@ -0,0 +1,3 @@
+export type ColorType = 'green' | 'blue' | 'yellow' | 'red' | 'teal';
+
+export type Position = 'top' | 'left' | 'bottom' | 'right';
