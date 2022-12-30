7018bf3cdcfa0ce281c287352298f9ddeeed15a0
Marek Fodor <fodormarek5@gmail.com>
2022-12-30 12:16:13 +0100

    Add one source of icons

---
+++ frontend/src/Assets/icons.tsx
@@ -0,0 +1,7 @@
+import { AiFillWarning } from 'react-icons/ai';
+import { BsPlusLg } from 'react-icons/bs';
+import { GrClose } from 'react-icons/gr';
+
+export const AddSymbol = BsPlusLg;
+export const CloseSymbol = GrClose;
+export const WarningSymbol = AiFillWarning;
