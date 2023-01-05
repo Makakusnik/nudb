1e5ee33f6e58bae1eed44e70550cc5acdf6f202e
Marek Fodor <fodormarek5@gmail.com>
2023-01-05 13:08:02 +0100

    Make Baseinput uncoupled

--- frontend/src/components/Input/BaseInput.tsx
+++ frontend/src/components/Input/BaseInput.tsx
@@ -1,7 +1,27 @@
-import type { DetailedHTMLProps, InputHTMLAttributes } from 'react';
+import type { DetailedHTMLProps, InputHTMLAttributes, Ref } from 'react';
 
-export type InputProps = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
+export type NativeInputProps = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
 
-export const Input = ({ ...other }: InputProps) => {
-  return <input {...other} />;
+// TODO Sprav addony na input (left, right) errory taktiez.
+// TODO Ak je required tak musi mat cervenu hviezdicku
+// TODO Pridaj pattern matching
+// TODO Pridaj volitelny native validation
+// TODO Vycucni base input do vlastnej classy, tu ich len nastyluj accordingly (horziont vertical)
+
+export interface BaseInputProps extends NativeInputProps {
+  nativeValidation?: boolean;
+  inputRef?: Ref<HTMLInputElement>;
+}
+
+export const BaseInput = function BaseInput({ nativeValidation, inputRef, ...props }: BaseInputProps) {
+  return (
+    <input
+      {...props}
+      ref={inputRef}
+      onInvalid={(e) => {
+        !nativeValidation && e.preventDefault();
+        props.onInvalid && props.onInvalid(e);
+      }}
+    />
+  );
 };
